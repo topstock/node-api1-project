bb8e147c85bb054e3fd68b3afa06aee60700abfd
Phil G <fillupgalbraith@gmail.com>
2022-01-10 17:33:29 -0800

    getting users and user by id

--- api/server.js
+++ api/server.js
@@ -1,3 +1,46 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+//IMPORTS
+const express = require('express');
+const {
+    find,
+    findById,
+    insert,
+    update,
+    remove,
+    resetDB, // ONLY TESTS USE THIS ONE
+  } = require('./users/model'); 
+//import express from 'express
+const server = express();
+
+server.use(express.json());  //parse.json from requests
+
+server.post('/users', async(req, res) => {
+  try {
+    await insert(req.body);
+    // const newUsers = await find();
+    // res.status(200).json(newUsers);
+  } catch(err) {
+    res.status(500).json({ message: err.message });
+  }
+})
+
+server.get('/users', async (req, res) => {
+  try {
+    const users = await find(); 
+    res.status(200).json(users);
+  } catch(err) {
+    res.status(500).json({message: err.message});
+  }
+});
+
+server.get('/users/:id', async (req,res) => {
+    try {
+        const user = await findById(req.params.id);
+        res.status(200).json( user );
+    } catch(err) {
+        res.status(500).json({message: err.message});
+    }
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require('./api/server');
 
 const port = 9000;
-
 // START YOUR SERVER HERE
+server.listen(port, () => {
+    console.log('listening for HTTPS requests from clients on port 9000')
+});
